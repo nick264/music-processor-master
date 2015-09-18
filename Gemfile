07b32f39930b479710a11a808d0103a4555594e1
Nick Sedlet <nicholas.sedlet@gmail.com>
2015-09-18 01:54:18 +0000

    stream audio to speaker

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ source "https://rubygems.org"
 gem 'rest-client'
 gem 'youtube-dl.rb'
 gem 'ruby-audio'
+gem 'plaything'
--- start.rb
+++ start.rb
@@ -2,6 +2,8 @@ load "init.rb"
 
 # code goes here
 
-wave_file = YoutubeAudio.fetch!("https://www.youtube.com/watch?v=b6YWZKfvpho")
+# filename = YoutubeAudio.fetch!("https://www.youtube.com/watch?v=b6YWZKfvpho")
+filename = File.join(ROOT_DIR,"cache/b6YWZKfvpho.wav")
+buf = nil;RubyAudio::Sound.open(filename) { |snd| 1000.times.each{ buf = snd.read(:float,1000); puts "#{buf.real_size}\t#{buf.to_a.flatten.max}"; plaything << buf.to_a.map{ |x| ( x[0] + x[1] ) } } }
 # chords = Chordify.fetch!("https://www.youtube.com/watch?v=b6YWZKfvpho")
 
