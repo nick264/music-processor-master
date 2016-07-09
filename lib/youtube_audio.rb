c1c05f14b88c241ad0cb029f018c8ba0d46f4ea6
Nick Sedlet <nicholas.sedlet@gmail.com>
2016-07-09 11:26:52 -0400

    youtube videos are now webms

--- lib/youtube_audio.rb
+++ lib/youtube_audio.rb
@@ -2,7 +2,7 @@ class YoutubeAudio
 	def self.fetch!(youtube_url, force=false)
 		name = CGI::parse(URI.parse(youtube_url).query)["v"][0]
 		fullpath = File.join(ROOT_DIR,"cache",name)
-		video_filename = fullpath + ".m4a"
+		video_filename = fullpath + ".m4a.webm"
 		audio_filename = fullpath + ".aac"
 		# wav_filename = fullpath + ".wav"
 		YoutubeDL.download youtube_url, output: video_filename if ( !File.exists?(audio_filename) && !File.exists?(video_filename) ) || force
