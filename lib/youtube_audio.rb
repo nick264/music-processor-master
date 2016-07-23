e443330a99f32a3523bb001994784eff57dceae5
Nick Sedlet <nicholas.sedlet@gmail.com>
2016-07-23 14:35:01 -0400

    fixing youtubeaudio for latest youtube-dl version; making it much faster

--- lib/youtube_audio.rb
+++ lib/youtube_audio.rb
@@ -2,14 +2,15 @@ class YoutubeAudio
 	def self.fetch!(youtube_url, force=false)
 		name = CGI::parse(URI.parse(youtube_url).query)["v"][0]
 		fullpath = File.join(ROOT_DIR,"cache",name)
-		video_filename = fullpath + ".m4a.webm"
+		yt_download_filename = fullpath + ".m4a"
 		audio_filename = fullpath + ".aac"
 		# wav_filename = fullpath + ".wav"
-		YoutubeDL.download youtube_url, output: video_filename if ( !File.exists?(audio_filename) && !File.exists?(video_filename) ) || force
-		`ffmpeg -i #{video_filename} -vn -acodec copy #{audio_filename}` if !File.exists?(audio_filename) || force
+		YoutubeDL.download youtube_url, output: yt_download_filename, format: 140 if ( !File.exists?(audio_filename) && !File.exists?(yt_download_filename) ) || force
+		`ffmpeg -i #{yt_download_filename} -vn -acodec copy #{audio_filename}` if !File.exists?(audio_filename) || force
 		# `ffmpeg -i #{audio_filename} #{wav_filename}` if !File.exists?(wav_filename) || force
 
-		`rm #{video_filename}`
+		`rm #{yt_download_filename}` if File.exists?(yt_download_filename)
+		raise "Didn't create audio successfully" if !File.exists?(audio_filename)
 		audio_filename
 	end
 end
